7250fde39ca5e35320ac91c5dbd05fd7879ec805
jayantiRana <100762037+jayantiRana@users.noreply.github.com>
2022-03-02 14:50:49 +0530

    First draft

---
+++ book-catalogue/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT into book (isbn, author, publish_date, title)values (12345678,'Authname1',SYSDATE,'BookTitle');
+INSERT into book (isbn, author, publish_date, title)values (22345678,'Authname2',SYSDATE,'BookTitle2');
+INSERT into book (isbn, author, publish_date, title)values (32345678,'Authname3',SYSDATE,'BookTitle3');
+INSERT into book (isbn, author, publish_date, title)values (42345678,'Authname4',SYSDATE,'BookTitle4');
